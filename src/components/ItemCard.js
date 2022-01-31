c897e152229f3f5082ee90b839a701efb50e8cc0
fatimashahid101 <fatimashahid600@gmail.com>
2022-01-31 22:38:16 +0500

    addtocart

---
+++ src/components/ItemCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useCart } from 'react-use-cart';
+export default function ItemCard(props) {
+    const {addItem} = useCart()
+    return (
+        <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
+            <div className="card p-0 overflow-hidden h-100 shadow">
+  <img className="card-img-top img-fluid" src={props.img} style={{height: "300px", width:"500px"}} />
+  <div className="card-body text-center">
+   <h5 className="card-title">{props.title}</h5>
+  <div className="card-text">    {props.desc}</div>
+  <h5 className="card-title">$ {props.price}</h5>
+
+ 
+    <button className="btn btn-success" onClick={() => addItem(props.item)}>Add to cart</button>
+    </div>
+</div>
+        </div>
+    );
+};
---
+++ src/components/data.js
@@ -0,0 +1,70 @@
+import image1 from "../assets/image1.jpg"
+import image2 from "../assets/image2.jpg"
+import image3 from "../assets/image3.jpg"
+import image4 from "../assets/image4.jpg"
+import image5 from "../assets/image5.jpg"
+import image6 from "../assets/image6.jpg"
+import image7 from "../assets/image7.jpg"
+import image8 from "../assets/image8.jpg"
+
+const data = {
+    productData: [
+        {
+            id: 1,
+            img : image1,
+            title: "juicy Apple",
+            desc: "aaaaaaaaaaaaaaaaaaaaaaaa",
+            price: "45"
+        },
+        {
+            id: 2,
+            img : image2,
+            title: "Banana",
+            desc: "aaaaaaaaaaaaaaaaaaaaaaaa",
+            price: "47"
+        },
+        {
+            id: 3,
+            img : image3,
+            title: "juicy orange",
+            desc: "aaaaaaaaaaaaaaaaaaaaaaaa",
+            price: "40"
+        },
+        {
+            id: 4,
+            img : image4,
+            title: "juicy Mango",
+            desc: "aaaaaaaaaaaaaaaaaaaaaaaa",
+            price: "50"
+        },
+        {
+            id: 5,
+            img : image5,
+            title: "Juicy Grapes",
+            desc: "aaaaaaaaaaaaaaaaaaaaaaaa",
+            price: "60"
+        },
+        {
+            id: 6,
+            img : image6,
+            title: "Watermelon",
+            desc: "aaaaaaaaaaaaaaaaaaaaaaaa",
+            price: "42"
+        },
+        {
+            id: 7,
+            img : image7,
+            title: "strawberries",
+            desc: "aaaaaaaaaaaaaaaaaaaaaaaa",
+            price: "30"
+        },
+        {
+            id: 8,
+            img : image8,
+            title: "Cherry",
+            desc: "aaaaaaaaaaaaaaaaaaaaaaaa",
+            price: "30"
+        },
+    ]
+}
+export default data;
